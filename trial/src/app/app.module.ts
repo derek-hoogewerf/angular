cbb2f7b9186545737a1dc10438157350a789008f
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-11 17:51:34 +0530

    Update app.module.ts

--- trial/src/app/app.module.ts
+++ trial/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { ListComponent } from './list/list.component';
 
+import { FormsModule } from '@angular/forms';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -14,7 +16,8 @@ import { ListComponent } from './list/list.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
