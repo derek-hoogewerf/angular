42492b0001440fc67a1286056e631a5d2ffc2218
Derek Hoogewerf <58857663+derek-hoogewerf@users.noreply.github.com>
2020-07-11 17:24:41 +0530

    Update app-routing.module.ts

--- trial/src/app/app-routing.module.ts
+++ trial/src/app/app-routing.module.ts
@@ -1,7 +1,8 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {HomeComponent} from './home/home.component';
-import {ListComponent} from './list/list.component';
+import { HomeComponent } from './home/home.component';
+import { ListComponent } from './list/list.component';
+import { FormsModule } from '@angular/forms';
 
 
 const routes: Routes = [
